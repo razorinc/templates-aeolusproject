5d268e9dca574a275d6a7ac375b001d7a9d1683c
Francesco Vollero <raven@lilik.it>
2012-07-25 11:32:29 +0200

    Added standard configuration constants

--- configuration.rb
+++ configuration.rb
@@ -2,3 +2,7 @@ ENV['GITHUB_KEY'] ||= 'Client ID'
 ENV['GITHUB_SECRET'] ||= 'Secret'
 ENV['TWITTER_KEY'] ||= 'Consumer Key'
 ENV['TWITTER_SECRET'] ||= 'Consumer Secret'
+DB_USER=ENV['DB_USER'] ||  "root"
+DB_PASSWORD=ENV['DB_PASSWORD'] ||  ""
+DB_SERVER=ENV['DB_SERVER'] ||  "localhost"
+DB_DATABASE=ENV['DB_DATABASE'] || "templates"
